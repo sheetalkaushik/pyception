1eadcb81d8a3288a9c0e36f939cde9772e34d4ad
Sheetal Kaushik <47242649+sheetalkaushik@users.noreply.github.com>
2019-02-04 23:59:06 +0530

    Update text_cleaning.py

--- text_cleaning.py
+++ text_cleaning.py
@@ -3,7 +3,10 @@ filename = 'test_clean.txt'
 file = open(filename, 'rt')
 text = file.read()
 file.close()
-# split based on words only
-import re
-words = re.split(r'\W+', text)
-print(words[:100])
+# split into words by white space
+words = text.split()
+# remove punctuation from each word
+import string
+table = str.maketrans('', '', string.punctuation)
+stripped = [w.translate(table) for w in words]
+print(stripped[:100])
